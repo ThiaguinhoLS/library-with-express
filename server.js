dc727da0af2a27741844e980834c00a317f0c4b3
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-12-26 01:52:38 -0200

    Alterado o server.js

---
+++ server.js
@@ -0,0 +1,69 @@
+/**
+  * File: server.js
+  * Date: 12-25-2018
+  * Description: Main file.
+  *
+  */
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = 3000;
+const book = require('./app/routes/book');
+const config = require('config');
+
+
+// Opções da base de dados
+const options = {
+  server: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 1,
+      connectTimeoutMS: 30000
+    }
+  }
+};
+
+// Conexão com a base de dados
+
+mongoose.connect(config.dbHost, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error'));
+
+if (config.util.getEnv('NODE_ENV') != 'test') {
+  app.use(morgan('combined'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// Routes
+app.get('/', (request, response, next) => {
+  response.json({
+    message: 'Welcome library'
+  });
+});
+
+app.route('/book')
+  .get(book.selectAllBooks)
+  .post(book.appendBook);
+
+
+app.route('/book/:id')
+  .get(book.selectBookForId)
+  .delete(book.removeBook)
+  .put(book.updateBook);
+
+app.listen(port);
+console.log('Application running on port 3000');
+module.exports = app;
+
