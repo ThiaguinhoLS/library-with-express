d1b6d2a666fcc1d7f240cccd7b88cc5509e91d09
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-12-27 20:55:27 -0200

    Adicionado o coveralls

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 *CRUD with express and mocha*
 
 [![Build Status](https://travis-ci.org/ThiaguinhoLS/library-with-express.svg?branch=master)](https://travis-ci.org/ThiaguinhoLS/library-with-express)
+
+[![Coverage Status](https://coveralls.io/repos/github/ThiaguinhoLS/library-with-express/badge.svg?branch=master)](https://coveralls.io/github/ThiaguinhoLS/library-with-express?branch=master)
--- server.js
+++ server.js
@@ -4,9 +4,7 @@
   * Description: Main file.
   *
   */
-
-const express = require('express');
-const app = express();
+const app = require('./app/app');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -15,7 +13,7 @@ const book = require('./app/routes/book');
 const config = require('config');
 
 
-// Opções da base de dados
+// Opções do database
 const options = {
   auth: {
     user: 'thiaguinhols',
@@ -24,39 +22,51 @@ const options = {
   useNewUrlParser: true
 };
 
-// Conexão com a base de dados
-
+// Conexão com o database
 mongoose.connect(config.dbHost, options);
 const db = mongoose.connection;
+
+// Caso ocorra um erro no database imprima no console
 db.on('error', console.error.bind(console, 'Connection error'));
 
+// Recupera a variável de ambiente
 if (config.util.getEnv('NODE_ENV') != 'test') {
   app.use(morgan('combined'));
 }
 
+// Define os middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-// Routes
+
+// Rotas
 app.get('/', (request, response, next) => {
   response.json({
     message: 'Welcome library'
   });
 });
 
+
+// Define as rotas de GET/ e POST/
 app.route('/book')
   .get(book.selectAllBooks)
   .post(book.appendBook);
 
 
+// Define as rotas de GET/:id, DELETE/:id e PUT/:id
 app.route('/book/:id')
   .get(book.selectBookForId)
   .delete(book.deleteBook)
   .put(book.updateBook);
 
-app.listen(port);
+
+// Inicia o servidor
+const server = app.listen(process.env.PORT || port);
+
 console.log('Application running on port 3000');
-module.exports = app;
+
+// Exporta o server
+module.exports = server;
 
